a13fcdf019362bd06ed3f06afc23df191e4637cb
Robert Nascimento <robert1a7x@gmail.com>
2022-02-04 15:17:48 -0300

    Configuraçao inicial - Criado o model e migration de Categorie

---
+++ models/categorie.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) => {
+  const Categorie = sequelize.define('Categorie', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    name: DataTypes.STRING,
+  }, { timestamps: false });
+
+  return Categorie;
+};

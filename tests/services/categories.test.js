2c43c311e00487fade868e2ffdfb413689c59b22
Robert Nascimento <robert1a7x@gmail.com>
2022-04-14 15:20:22 -0300

    Testes da rota categories

---
+++ tests/services/categories.test.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai');
+const Sinon = require('sinon');
+const { create, getAll } = require('../../services/categorieService');
+const { Categorie } = require('../../models');
+const { categoriesMock } = require('../services/mocks');
+
+describe('Testes das categories', () => {
+  describe('Método getAll()', () => {
+    before(() => {
+      Sinon.stub(Categorie, 'findAll').resolves(categoriesMock);
+    });
+
+    after(() => {
+      Sinon.restore();
+    });
+
+    it('deve retornar uma lista de categorias', async() => {
+      const categories = await getAll();
+      expect(categories).to.deep.eq(categoriesMock);
+    });
+  });
+
+	describe('Método create()', () => {
+    describe('Quando criado com sucesso', () => {
+			before(() => {
+				Sinon.stub(Categorie, 'create').resolves({id: 1, name: "Inovação" });
+			});
+	
+			after(() => {
+				Sinon.restore();
+			});
+	
+			it('deve retornar um objeto com a categoria criada', async() => {
+				const newCategory = await create({ name: "Inovação" });
+				expect(newCategory).to.deep.eq({id: 1, name: "Inovação" });
+			});
+		});
+
+		describe('Quando nao é passado o atributo name na requisicao', () => {
+			it('deve retornar um objeto de erro, com o erro 400', async() => {
+				const newCategory = await create({});
+				expect(newCategory).to.deep.eq({ errCode: 400, message: '"name" is required' });
+			});
+		});
+
+  });
+});
---
+++ tests/services/mocks.js
@@ -0,0 +1,14 @@
+const categoriesMock = [
+  {
+    id: 1,
+    name: "Escola"
+  },
+  {
+    id: 2,
+    name: "Inovação"
+  }
+];
+
+module.exports = {
+	categoriesMock,
+}
